45e827f9931827a251c48d2a1174e1325a17ae1d
Andarios910 <andarioskarokaro92@gmail.com>
2023-07-08 15:06:52 +0700

    create routes

---
+++ router/route.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router = Router();
+
+/** Questions Routes API */
+router.get('/questions', (req, res) => {
+    res.json('questions api get request');
+})
+
+
+
+export default router;
